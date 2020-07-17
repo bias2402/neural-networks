bb0427702ecac2f4d91bdeb473af3a2fa2de94de
bias2402 <bias2402@hotmail.dk>
2020-07-17 23:58:13 +0200

    Visualization startup
    I've made three scripts and some prefabs that should suffice for the visualization. This will be a bit hard to set up, and I might need a scriptable object already, but hey(!), I must learn them at some point!

---
+++ Assets/Scripts/ConnectionVisualization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionVisualization : MonoBehaviour {
+    [SerializeField] private Image connectionImage = null;
+
+    void Start() {
+        if (connectionImage == null) {
+            connectionImage = GetComponent<Image>();
+        }
+    }
+    public void SetPosition(Transform start, Transform end) {
+
+    }
+}
---
+++ Assets/Scripts/NeuronVisualization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeuronVisualization : MonoBehaviour {
+    [SerializeField] private Image neuronImage = null;
+
+    void Start() {
+        if (neuronImage == null) {
+            neuronImage = GetComponent<Image>();
+        }
+    }
+    public void SetPosition() {
+
+    }
+}
